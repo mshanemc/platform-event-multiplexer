87d7809fd87c6fd969af658449f61d628c1c1b51
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-07-10 14:31:04 -0500

    add CORS *

--- app.js
+++ app.js
@@ -13,10 +13,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 /* GET home page. */
 app.get('/', function (req, res, next) {
   res.render('index', { title: 'Platform Event Multiplexer' });
 });
+
 
 // post orgId, sessionId, serverURL
 app.post('/sessionId', jsonParser, function (req, res, next) {
